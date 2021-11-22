f5f2089daf157258590276108737d447da80c9ec
browissimo <browissimo@gmail.com>
2021-11-22 17:27:44 +0300

    first commit

---
+++ AllAndNothing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AllAndNothing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветсвую, Друг, чем займемся на этот раз?\n");
+
+            ProgramList programList = new ProgramList();
+
+            foreach (KeyValuePair<int, string> keyValue in programList.programDict)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+
+            Console.WriteLine("\nВведити цифру соответсвующую соответствующую требуемому функционалу");
+
+            bool rez = int.TryParse(Console.ReadLine(), out int num);
+            if (rez)
+            {
+                switch (num)
+                {
+                    case 1 :  
+                        PiLenthToN.InN();
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Введите корректное значение");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ AllAndNothing/ProgramList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllAndNothing
+{
+    public class ProgramList
+    {
+        public Dictionary<int, string> programDict = new Dictionary<int, string>
+        {
+            {1, "Найти число pi до n-й цифры после запятой" }
+        };
+    }
+}
